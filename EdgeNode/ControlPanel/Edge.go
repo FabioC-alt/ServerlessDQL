58bfc82ea5e60ebe594e91415dcb4834d508ff28
FabioC-alt <fabiociraci41@gmail.com>
2025-10-16 17:52:51 +0200

    Deployment

--- EdgeNode/ControlPanel/Edge.go
+++ EdgeNode/ControlPanel/Edge.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	
-	"os"
-    "time"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func main() {
-
-	appEnv := os.Getenv("APP_ENV")
-	Ports := os.Getenv("PORTS")
-
+	inferURL := "http://edgenodepanel:8080/infer"
 	i := 1
 	for {
-		fmt.Printf("Loop %d - App Environment: %s\n", i, appEnv)
-		fmt.Printf("Loop %d - Ports: %s\n", i, Ports)
+		// Example payload
+		payload := map[string]interface{}{
+			"input": fmt.Sprintf("test-%d", i),
+		}
+		body, err := json.Marshal(payload)
+		if err != nil {
+			fmt.Printf("Error marshaling JSON: %v\n", err)
+			continue
+		}
+		resp, err := http.Post(inferURL, "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			fmt.Printf("Request error: %v\n", err)
+		} else {
+			respBody, _ := ioutil.ReadAll(resp.Body)
+			fmt.Printf("Loop %d - Response: %s\n", i, string(respBody))
+			resp.Body.Close()
+		}
 		i++
 		time.Sleep(2 * time.Second)
 	}
